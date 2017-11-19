c643cf5bc19add2545f59f9ca3d7c602a8020675
aenun050859 <ae_baw1122@hotmail.com>
2017-11-19 09:49:21 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110004/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110004.rmutsv.com;
+
+public class HelloWorld
+{
+//    Create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world, I'm AE");
+    System.out.println("My name is Kanyarat Songsri");
+
+
+
+}//main
+
+}// class
