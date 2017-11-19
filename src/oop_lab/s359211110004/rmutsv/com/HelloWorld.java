3f4e1d3be856ac6483efa8df8c7513ec166b3aa7
aenun050859 <ae_baw1122@hotmail.com>
2017-11-19 10:36:51 +0700

    Testing delete Tnformation

--- src/oop_lab/s359211110004/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110004/rmutsv/com/HelloWorld.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
     System.out.println("Hello world, I'm AE");
     System.out.println("My name is Kanyarat Songsri");
 //    add information
-    System.out.println("Faculty: Management Technology");
-    System.out.println("RMUTSV");
+    System.out.println("Faculty: Management Technology,RMUTSV");
+
 
 
 
