5b9e6173834ea592b612bf24316dee388b7d534a
aenun050859 <ae_baw1122@hotmail.com>
2017-11-27 14:58:59 +0700

    TestCondition

---
+++ src/ooplab3/TestCondition.java
@@ -0,0 +1,32 @@
+package ooplab3;
+
+public class TestCondition {
+}
+
+    public static void main(String[] args) {
+//        if
+        int x = 10, y = 20;
+        if(x<y)
+            System.out.println("x less than y");
+        else
+            System.out.println("x more than y ");
+
+//        if-else-if
+        if (y>30){
+            System.out.println("y less than 30");
+        }
+        else if(y>=20){
+            System.out.println("y less e 20");
+        }
+        else{
+            System.out.println("y equal 20");
+        }
+
+
+
+
+
+
+
+
+  }//main
