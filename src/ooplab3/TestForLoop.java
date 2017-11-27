e2f5e0a46494b6b0901cca6882f3c1db5455f678
aenun050859 <ae_baw1122@hotmail.com>
2017-11-27 16:39:03 +0700

    Input Score with Grade

--- src/ooplab3/TestForLoop.java
+++ src/ooplab3/TestForLoop.java
@@ -3,8 +3,10 @@ package ooplab3;
 public class TestForLoop {
     public static void main(String[] args) {
 
-        for (int i =1;i<=100;i++)
-            System.out.print(""+i);
+        for (int i =1;i<=100;i++) {
+            if (i%3==0||i%5==0||i%7==0)
+                System.out.print(i+" ");
+        }//for
 
 
 
