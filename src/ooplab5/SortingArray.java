b671a16573cf7210f52650a4ca01e912d5b0e0e9
aenun050859 <ae_baw1122@hotmail.com>
2017-12-25 15:08:14 +0700

    myArray2D

---
+++ src/ooplab5/SortingArray.java
@@ -0,0 +1,14 @@
+package ooplab5;
+//1. allow user inout data into array
+//2. fine summation of data in array
+//3. find average value in array
+//4. find maximum value in array
+//5. find minimum value in array
+//6. sorting data in array (Descending order)
+//7. sorting data in array (Ascending order)
+
+public class SortingArray {
+    public static void main(String[] args) {
+
+    }//main
+}//class
