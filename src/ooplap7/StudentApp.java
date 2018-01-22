e5356b134f7c5499733450f02a285f59f8a78d42
aenun050859 <ae_baw1122@hotmail.com>
2018-01-22 14:46:53 +0700

    Student and App

--- src/ooplap7/StudentApp.java
+++ src/ooplap7/StudentApp.java
@@ -1,5 +1,7 @@
 package ooplap7;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
         //create object as Student
@@ -16,7 +18,26 @@ public class StudentApp {
         showData(s1);
         showData(s2);
 
+        Student s3 = new Student();
+        s3 = inputData(s3);
+
     }//main
+
+    private static Student inputData(Student s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter student info:");
+        System.out.print("Enter student ID: ");
+        s.setId(scanner.nextLine());
+        System.out.print("Enter student name: ");
+        s.setname(scanner.nextLine());
+        System.out.println("Enter student major: ");
+        s.setmajor(scanner.nextLine());
+        System.out.println("Enter student group: ");
+        s.setgroup(scanner.nextLine());
+
+        return s;
+    }//inputData
+
 
     private static void showData(Student s) {
         System.out.println("Student info: ");
